d07bbcd21e5a2273083c4f0a2d63b3a7f8cbb17c
David Zeiser <davidzeiser@gmail.com>
2016-12-19 18:17:13 -0800

    fixes

--- GameController.cs
+++ GameController.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using UnityStandardAssets.ImageEffects;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
@@ -13,8 +14,9 @@ public class GameController : MonoBehaviour {
 
 	private bool gameEnding = false;
     private float fadeAmount = 1.0f;
-    private GameObject[] colorObjects;
-    private float colorDistance = 3.5f;
+    private List<GameObject> colorObjects = new List<GameObject>();
+    private float colorDistance = 4.5f;
+    private float colorDistanceOffset = 1.0f;
     // Use this for initialization
     void Start () {
         player = GameObject.Find("Character");        
@@ -23,7 +25,10 @@ public class GameController : MonoBehaviour {
         canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
         Time.timeScale = 1.0f;
 		Time.fixedDeltaTime = 0.02F * Time.timeScale;
-        colorObjects = GameObject.FindGameObjectsWithTag("colorable");
+        //Populate color object list, probably a better way to do this
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("colorable");
+        foreach (GameObject obj in temp)        
+            colorObjects.Add(obj);
     }
 	
 	// Update is called once per frame
@@ -48,17 +53,26 @@ public class GameController : MonoBehaviour {
     }
         
     void colorScene()
-    {
-        for(int i = 0;i < colorObjects.Length;i++)
+    {        
+        for (int i = 0;i < colorObjects.Count;i++)
         {
-            Vector3 charPos = player.transform.position;
-            float distance = Vector3.Distance(charPos, colorObjects[i].transform.position);            
+            Vector2 charPos = new Vector2(player.transform.position.x, player.transform.position.y);
+            Vector2 objectPos = new Vector2(colorObjects[i].transform.position.x, colorObjects[i].transform.position.y);
+            float distance = Vector2.Distance(charPos, objectPos);            
             if (distance > colorDistance) //Goto next iteration if object outside minimum color distance
                 continue;
 
-            float alpha = (distance / colorDistance);
-            colorObjects[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), alpha);            
-        }
+            float alpha = (distance - colorDistanceOffset) / colorDistance;
+            colorObjects[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), alpha);
+            if (alpha <= .15f) //if over 85% colored then make 100% and remove from list
+            {
+                colorObjects[i].GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), 0);
+                colorObjects.RemoveAt(i);
+                //Debug.Log("Removing object: " + i);
+                continue;
+            }
+                      
+        }        
     }
 
     //Restart current scene
