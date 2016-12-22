d85d5f05a3d078166ed449d2e548170441de5623
max volz <max volz>
2016-12-21 22:15:55 -0800

    level selection script that needs to be changed

---
+++ LevelSelection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			Application.LoadLevel (levelName);
+		}
+	}
+
+
+}
