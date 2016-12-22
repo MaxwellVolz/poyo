569ac65dbd0989b4e853525c38afb4bd755a5a44
David Zeiser <davidzeiser@gmail.com>
2016-12-21 23:20:07 -0800

    fixes

---
+++ LevelSelection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour
+{
+    GameController gc;
+    
+    void Start()
+    {
+        gc = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+    
+    void Update()
+    {        
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                //Debug.Log(hit.transform.name);
+                gc.changeLevel(hit.transform.name);
+            }
+        }
+    }   
+}
